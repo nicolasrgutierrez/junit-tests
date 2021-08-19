a17930dcd08c8b6462413188b61642140cabfcb4
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-08-19 11:49:08 -0500

    had issues with git, should work now

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,35 +1,37 @@
+package java;
+
 import org.junit.Test;
+
+import java.Student;
+
 import static org.junit.Assert.*;
 
 public class StudentTest {
 
     @Test
     public void testIfStudentCanBeCreated(){
-        Student fer = new Student(1L, "fer");
+        Student john = new Student(1L, "john");
         Student nullStudent = null;
-        assertNotNull(fer);
+        assertNotNull(john);
         assertNull(nullStudent);
     }
 
     @Test
     public void testIfFieldsAreSet(){
-        Student fer = new Student(1L, "fer");
-        assertEquals(1L, fer.getId());
-        assertEquals("fer", fer.getName());
+        Student idris = new Student(1L, "idris");
+        assertEquals(1L, idris.getId());
+        assertEquals("idris", idris.getName());
 
-        Student rosie = new Student(40L, "Rosie");
-        assertEquals(40L, rosie.getId());
-        assertEquals("Rosie", rosie.getName());
     }
 
     @Test
     public void testIfAddGradeWorks(){
-        Student fer = new Student(1L, "fer");
-        assertEquals(0, fer.getGrades().size());
-        fer.addGrade(90);
-        assertEquals(1, fer.getGrades().size());
-        fer.addGrade(70);
-        assertEquals(2, fer.getGrades().size());
+        Student harley = new Student(1L, "harley");
+        assertEquals(0, harley.getGrades().size());
+        harley.addGrade(90);
+        assertEquals(1, harley.getGrades().size());
+        harley.addGrade(70);
+        assertEquals(2, harley.getGrades().size());
     }
 
     @Test
@@ -43,12 +45,12 @@ public class StudentTest {
 
     @Test
     public void testIfGetAVGWorks(){
-        Student fer = new Student(1L, "fer");
-        fer.addGrade(90);
-        fer.addGrade(70); // 160 / 2 = 80
-        assertEquals(80, fer.getGradeAverage(), 0);
-        fer.addGrade(50); // 210 / 3 = 70
-        assertEquals(70, fer.getGradeAverage(), 0);
+        Student shark = new Student(1L, "shark");
+        shark.addGrade(90);
+        shark.addGrade(70);
+        assertEquals(80, shark.getGradeAverage(), 0);
+        shark.addGrade(50);
+        assertEquals(70, shark.getGradeAverage(), 0);
     }
 
 }
